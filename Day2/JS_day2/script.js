fbbbbdf32c0fcc992b71547ffc085b40f8b39634
Bach Vu <bachvu6198@gmail.com>
2022-06-10 15:45:54 +0700

    Bosung note buoi hoc 2"

---
+++ Day2/JS_day2/script.js
@@ -0,0 +1,82 @@
+
+//Toán tử (Operators)
+//Có 5 loại:
+// 1. Arithmetic
+// 2. Comparision
+// 3. Assignment
+// 4. Logical
+// 5. Bitwise
+
+//1. Với phép +
+// Nếu 1 trong 2 toán hạng (giá trị) có kiểu chuỗi,
+// thì giá trị còn lại cũng chuyển thành kiểu chuỗi
+
+//2. Các tóan tử toán học khác (- * / ...)
+// thì ưu tiên chuyển sang số
+
+// Có 2 cách viết / cách sử dụng
+//Kết quả khác nhau khi sử dụng trong 1 biểu thức
+let a = 1;
+
+// Sử dụng ở phía sau tên biến
+//a++;
+//a--;
+
+//Thực hiện phép tính trước
+// hiện tại của biến sau đó mới tăng
+
+let result = 10 + a--; //11, a => 0
+console.log(result);
+
+
+//Sử dụng ở trước tên biến
+// ++a;
+// --a;
+
+//Tăng giá trị của biến trước, sau đó mới tính
+let result = 10 + ++a; // a => 2, 12
+console.log(result);
+
+
+
+//equal == & != (tự động chuyển đổi)
+//strict equal === !==
+//Không chuyển đổi & so sánh cả kiểu dữ liệu (SO SÁNH CẢ KIỂU DỮ LIỆU)
+
+//1. Các toán tử ++ --
+//2. == === != !==
+
+
+"B".charCodeAt()
+
+
+// NOT - ! (phủ định) - Chuyển đổi giá trị về kiểu logic vào đđảo ngược
+
+//AND - && (và)
+//Đánh giá toán hạng (biểu thức) thành giá trị logic
+//Trả về kết quả đầu tiên là false
+//Nếu k có toán dạng nào là false => trả về thằng cuối cùng
+
+
+//OR - || (hoặc)
+//Đánh giá toán hạng (biểu thức) thành giá trị logic
+//Trả về kết quả đầu tiên là true
+//Nếu không có thằng nào là true => trả về thằng cuối cùng
+
+
+
+
+
+1. 10
+
+2. 
+
+3. 1
+
+4. "2"
+
+5. "6"
+
+6. "9px"
+
+7. 
