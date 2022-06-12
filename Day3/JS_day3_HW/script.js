cfced69105a3e0b6b44292f40aaec1f5f7c4d9b5
Bach Vu <bachvu6198@gmail.com>
2022-06-12 23:30:55 +0700

    xong level 1"

--- Day3/JS_day3_HW/script.js
+++ Day3/JS_day3_HW/script.js
@@ -1,3 +1,4 @@
+//Level 1
 //Bài 1
 let isOddNumber = function (number) {
   if (number % 2 == 0) {
@@ -22,15 +23,44 @@ let divisibleByThreeAndFive = function (number) {
 
 // Bài 4
 let season = function (month) {
-  month;
+  if (month == 12 || month == 1 || month == 2) {
+    console.log("Winter");
+  } else if (month == 3 || month == 4 || month == 5) {
+    console.log("Spring");
+  } else if (month == 6 || month == 7 || month == 8) {
+    console.log("Summer");
+  } else if (month == 9 || month == 10 || month == 11) {
+    console.log("Fall");
+  } else {
+    console.log("Invalid season");
+  }
 };
 
 //Bài 5
 let trafficLight = function (light) {
-  if (light == "Red") console.log("Dừng lại");
-  else if (light == "Green") console.log("Được phép di chuyển");
-  else if (light == "Yellow") console.log("Giảm tốc độ và dừng lại");
-  else console.log("Vui lòng nhập Red, Green, hoặc Yellow");
+  light = light.toLowercase();
+
+  if (light === "red") console.log(light);
+  else if (light === "green") console.log("Được phép di chuyển");
+  else if (light === "yellow") console.log("Giảm tốc độ và dừng lại");
+  else console.log("Nhập tín hiệu đèn Red, Green hoặc Yellow!");
 };
 
+//Bài 6
+let calcCommissions = function (totalSales) {
+  let commission;
+  if (totalSales > 300000000) {
+    commission = totalSales * 0.2;
+    console.log(`Tiền hoa hồng là ${commission}`);
+  } else if (totalSales <= 300000000 && totalSales > 100000000) {
+    commission = totalSales * 0.1;
+    console.log(`Tiền hoa hồng của bạn là ${commission}`);
+  } else {
+    commission = totalSales * 0.05;
+    console.log(`Tiền hoa hồng của bạn là ${commission}`);
+  }
+};
 
+calcCommissions(100000000);
+
+//Level 2
