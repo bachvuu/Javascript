d7eb43fd36ae944ae2d1826142adb0ceaf14780f
Bach Vu <bachvu6198@gmail.com>
2022-06-13 19:26:44 +0700

    bo sung bai 6

--- Day3/JS_day3_HW/script.js
+++ Day3/JS_day3_HW/script.js
@@ -108,3 +108,25 @@ let findMaxOfThree = function (a, b, c) {
 };
 
 //Bài 6
+function solveEquation(a, b, c) { 
+  if (a == 0 && b == 0 || c == 0) {
+    return Infinity
+  }
+
+  else if (a == 0 && b == 0 && c != 0) {
+    return null
+  }
+
+  else if (a == 0 && b != 0) {
+    return -c / b
+  }
+  else if (a != 0) { 
+    const delta = b ** 2 - 4 * a * c;
+    if (delta < 0) return nulll
+
+    const square = delta ** 0.5;
+
+    return [(-b + square) / (2 * a), (-b - square) / ]
+  }
+
+}
